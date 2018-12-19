f237963c54095917ad3cd08ccddf4e30c8349e7c
Jddanan <jddanan@gmail.com>
2018-12-19 17:33:09 +0200

    adding route for browse and search

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 import os
-from bottle import (get, post, redirect, request, route, run, static_file,
-                    template)
+from bottle import (get, post, redirect, request, route, run, static_file, error, template)
 import utils
 
 # Static Routes
+
 
 @get("/js/<filepath:re:.*\.js>")
 def js(filepath):
     return static_file(filepath, root="./js")
 
+
 @get("/css/<filepath:re:.*\.css>")
 def css(filepath):
     return static_file(filepath, root="./css")
+
 
 @get("/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="./images")
 
+
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+
+
+@route('/browse')
+def browse():
+    sectionTemplate = "./templates/browse.tpl"
+    sectionData = ""
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+@route('/browse/show/')
+def browse():
+    sectionTemplate = "./templates/show.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+@route('/search')
+def search():
+    sectionTemplate = "./templates/search.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+@error(404)
+def error404(error):
+    sectionTemplate = "./templates/404.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
 
 #run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
 run(host='localhost', port=7000)
